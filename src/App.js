042ab34b36529517e78ef2d8fbf4b004f8528e53
Chintankuma <chintanvaghani555@gmail.com>
2020-08-15 16:31:50 +0200

    input component completed

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ import TodoInput from "./components/TodoInput";
 class App extends Component {
   state = {
     items: [
-      { id: 1, title: "wake up" },
-      { id: 2, title: "morning break fast" },
+      // { id: 1, title: "wake up" },
+      // { id: 2, title: "morning break fast" },
     ],
     id: uuidv4(),
     item: "", //ifs for input item first leer
@@ -19,9 +19,32 @@ class App extends Component {
 
   handleChange = (e) => {
     console.log("handle change");
+    const item = e.target.value;
+    // console.log(item);
+    this.setState({
+      item: e.target.value,
+    });
   };
   handleSubmit = (e) => {
+    e.preventDefault();
     console.log("handle submit");
+    const newItem = {
+      id: this.state.id,
+      title: this.state.item,
+    };
+    const updatedItem = [...this.state.item, newItem];
+    // console.log(updatedItem);
+    this.setState(
+      {
+        items: updatedItem,
+        id: uuidv4(),
+        item: "",
+        editItem: false,
+      },
+      () => {
+        console.log(this.state);
+      }
+    );
   };
   clearList = () => {
     console.log("clear list");
